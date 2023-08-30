40d5007e6c331d1535590f5bc2bc5072c549153a
Karim Tilouni Diaz <117732405+KarimTDiaz@users.noreply.github.com>
2023-08-30 11:00:04 +0100

    with onrender

--- client/src/constants/requests.js
+++ client/src/constants/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/';
+const API_URL = 'https://other-life-store.onrender.com/';
 
 export const URLS = {
 	NEW_USER: API_URL + 'other-life-store/users/new-user',
